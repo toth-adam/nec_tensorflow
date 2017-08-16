1435df26d67d164ad04320bab7a90edea2cfecae
istvanmo <mo.istvan@gmail.com>
2017-08-16 16:37:13 +0200

    changed LRU to deque

--- replay_memory.py
+++ replay_memory.py
@@ -1,25 +1,20 @@
-from lru import LRU
+from collections import deque
 import numpy as np
 
-class replay_memory:
 
+class ReplayMemory:
     def __init__(self, size=5e5):
-        self.rep_mem = LRU(int(size))
+        self.rep_mem = deque(maxlen=size)
         self.size = size
 
     def append(self, item):
-        if len(self.rep_mem) < self.size:
-            num = len(self.rep_mem) #increment the key's number
-        else:
-            # get the last element's key and in the next step overwrite its value
-            num = self.rep_mem.peek_last_item()[0]
-        self.rep_mem[num] = item
+        self.rep_mem.append(item)
 
-    def get(self, batch_size):
+    def get_batch(self, batch_size):
         sample_list = []
-        # kirandomol batch size darab keyt
-        rand_samp_num = np.random.choice(range(len(self.rep_mem)), size=batch_size, replace=False)
-        # kiveszi a kirandomolt keyekhez tartozó értékeket az LRU-ból
+        # kirandomol batch size darab sorszámot
+        rand_samp_num = np.random.choice(range(self.size), size=batch_size, replace=False)
+        # kiveszi a kirandomolt sorszámhoz tartozó értékeket az LRU-ból
         for i in rand_samp_num:
             sample_list.append(self.rep_mem[i])
-        return sample_list
+        return np.asarray(sample_list)
